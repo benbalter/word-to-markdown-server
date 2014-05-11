96ecd560bac64ec820bd9d37517e5b7bc1ef17e0
Ben Balter <ben.balter@github.com>
2014-05-11 19:10:42 -0400

    update server to 1.0.0

--- server.rb
+++ server.rb
@@ -2,6 +2,7 @@ require 'word-to-markdown'
 require 'sinatra'
 require 'html/pipeline'
 require 'rack/coffee'
+require 'tempfile'
 
 module WordToMarkdownServer
   class App < Sinatra::Base
@@ -13,18 +14,21 @@ module WordToMarkdownServer
     end
 
     post "/" do
-      if params['doc'][:filename].match /docx?$/
-        error = "It looks like you tried to upload a Word Document. You must first export it as a web page."
+      unless params['doc'][:filename].match /docx?$/
+        error = "It looks like you tried to upload something other than a Word Document."
         render_template :index, { :error => error }
       end
       md = WordToMarkdown.new(params['doc'][:tempfile]).to_s
       html = HTML::Pipeline::MarkdownFilter.new(md).call
-      render_template :display, { :md => md, :html => html, :filename => params['doc'][:filename].sub(/\.html?$/,"") }
+      render_template :display, { :md => md, :html => html, :filename => params['doc'][:filename].sub(/\.docx?$/,"") }
     end
 
     post "/raw" do
-      html = request.env["rack.request.form_vars"]
-      WordToMarkdown.new("<!-- Prevent arbitrary file reads -->#{html}").to_s
+      file = Tempfile.new('word-to-markdown')
+      file.write request.env["rack.request.form_vars"]
+      WordToMarkdown.new(file.path).to_s
+      file.close
+      file.unlink
     end
 
     def render_template(template, locals={})
